d11982cddcae9083caafc52be6a2842a802fd586
Alex Goodwin <agoodwin727@gmail.com>
2015-03-31 17:48:56 -0600

    Notifications Updates

--- notifications.php
+++ notifications.php
@@ -11,167 +11,29 @@
 		<a href="#" class="active">You</a>
 	</nav>
 	
+	<?
+		$notifs = array();
+		
+		for($i=1;$i<=16;$i++){
+			$notifs[] = array(
+				'username' => 'thecolt',
+				'profile' => 'img/profiles/'.$i.'.jpg',
+				'message' => 'took a photo of you.',
+				'time' => round($i * rand(0,20)/3) . 'm',
+				'preview' => 'img/butts/butt'.$i.'.jpg'
+			);
+		}
+		
+	?>
+	
 	<ul class="notifs">
+		<? foreach($notifs as $key => $value): ?>
 		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
+			<img src="<? echo $value['profile'];?>" class="prof">
+			<span><a href="#"><? echo $value['username'];?></a> <? echo $value['message'];?> <i><? echo $value['time'];?></i></span>
+			<img src="<? echo $value['preview'];?>" class="preview">
 		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
-		<li>
-			<img src="img/wuddupdokProf.jpg" class="prof">
-			<span><a href="#">thecolt</a> took a photo of you. <i>20h</i></span>
-			<img src="img/belfie.JPG" class="preview">
-		</li>
+		<? endforeach; ?>
 	
 </section>
 
